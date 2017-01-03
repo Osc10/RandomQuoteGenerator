e500999c9ff222d274c2b331a22efcfedada4f3a
Oscar <oscarguoyinchen@gmail.com>
2017-01-03 22:08:16 +0000

    changed css and added animations

---
+++ animations.js
@@ -0,0 +1,45 @@
+$(".btn").hide();
+
+$(function() {
+    $.ajax({
+            headers: {"X-Mashape-Key": "6zKSOPpM4JmshXHfdNYW9J5ZGUaRp1HMB3ljsnDYjFJMcGoMGZ"} ,
+            url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+            success: function(data) {
+                object = JSON.parse(data);
+                $("#author").hide().html("- " + object["author"]).fadeIn(1000);
+                $("#quote").hide().html(object["quote"]).fadeIn(1000);
+                $(".btn").fadeIn(1000)
+            },
+
+        });
+
+    $(".btn").on("click", function() {
+   		$.ajax({
+            headers: {"X-Mashape-Key": "6zKSOPpM4JmshXHfdNYW9J5ZGUaRp1HMB3ljsnDYjFJMcGoMGZ"} ,
+            url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+            success: function(data) {
+                window.object = JSON.parse(data);
+            },
+
+        });
+//superlight, dark: purple, pink, red, light-blue, green, yellow, orange
+        var colors = [["#E8D7F7", "#300F4F"], ["#F7D5F5", "#590454"], ["#FCD9DE", "#5C000C"], ["#DEF5FA", "#02404D"], ["#D7FAD7", "#004501"], ["#F5F3CB", "#757206"], ["#F7DABA", "#5E3302"]] 
+                var rand = colors[Math.floor(Math.random() * colors.length)]
+      
+        $("#quote, #author").fadeOut(500, function() {
+         	$("#author").html("- " + window.object["author"]).fadeIn(500);
+         	$("#quote").html(window.object["quote"]).fadeIn(500);
+     	});
+
+        while (JSON.stringify(rand) == JSON.stringify(window.check)) {
+        	rand = colors[Math.floor(Math.random() * colors.length)];
+        }
+
+        window.check = rand;
+        $(".container-fluid").css("color", rand[1]);
+        $("button").css("color", rand[0]);
+     	$("body").css("background", rand[1]);
+     	$(".container-fluid").css("background", rand[0]);
+        $("button").css("background", rand[1]);
+    });
+});
